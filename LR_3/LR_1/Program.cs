902d6c4937cd708d666551c730ff4bcb1e5b70bb
AlenaPakhomova <alena.pahomova.99@mail.ru>
2023-04-20 21:29:43 +0700

    новое

---
+++ LR_3/LR_1/Program.cs
@@ -0,0 +1,90 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace LR_3
+{
+    /// <summary>
+    /// Класс для выполнения программы
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args">параметры</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("Creating seven people...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            PersonList personList = new PersonList();
+
+            for (int i = 1; i < 9; i++)
+            {                
+                personList.AddPerson(RandomPerson.GreateRandomPerson());
+            }
+
+            Console.WriteLine("The list has been created.");
+            PrintList(personList);
+            Console.ReadKey();
+
+            Console.WriteLine("\nInformation about the fourth person from" +
+                " the created list:");
+            Console.ReadKey();
+
+            Console.WriteLine("\nLet's find out about his taste " +
+                "preferences");
+            Console.ReadKey();
+
+            PersonBase person = personList.FindPersonByIndex(4);
+            switch (person)
+            {
+                case Adult adult:
+                    {
+                        Console.WriteLine(adult.FavoriteFood());
+                        break;
+                    }
+                case Child child:
+                    {
+                        Console.WriteLine(child.FavoriteSweet());
+                        break;
+                    }
+                default:
+                    break;
+            }          
+        }
+
+        /// <summary>
+        /// Печать списка
+        /// </summary>
+        /// <param name="people">человек</param>
+        public static void PrintList(PersonList people)
+        {
+            int count = people.CountPersonInList();
+            for (int i = 1; i < count; i++)
+            {
+                Console.WriteLine($"\nPerson № {i}");
+                Console.WriteLine(people.FindPersonByIndex(i).GetInfo());
+            }
+        }
+    }
+}
+
+
+
+
+        
+  
+      
+    
---
+++ LR_3/Model/PersonList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Model
+{
+    /// <summary>
+    /// Класс, описывающий абстракцию списка, 
+    /// содержащего объекты класса персон
+    /// </summary>
+    public class PersonList
+    {
+        /// <summary>
+        /// Список людей
+        /// </summary>
+        public List<PersonBase> people = new List<PersonBase>();
+
+        /// <summary>
+        /// Добавление людей
+        /// </summary>
+        /// <param name="person">люди</param>
+        public void AddPerson(PersonBase person)
+        {
+            people.Add(person);
+        }
+
+        /// <summary>
+        /// Удаление элементов (имён)
+        /// </summary>
+        /// <param name="name">имя</param>
+        /// <returns>Удаление людей</returns>
+        public int RemovePersonByName(string name)
+        {
+            int deleteName = people.RemoveAll(n => n.Name == name);
+            return deleteName;
+        }
+
+        /// <summary>
+        /// Удаление элементов из списка по индексу
+        /// </summary>
+        /// <param name="index">индекс элемента</param>
+        ///  <exception cref="IndexOutOfRangeException">Элементы с
+        ///  несуществующими индексами</exception>
+        public void RemovePersonByIndex(int index)
+        {
+            int indexNumber = people.Count - 1;
+            if (indexNumber < index)
+            {
+                throw new IndexOutOfRangeException($"Элемента с индексом" +
+                    $" {index} не существует");
+            }
+            else
+            {
+                people.RemoveAt(index);
+            }
+        }
+
+        /// <summary>
+        /// Поиск элемента по индексу
+        /// </summary>
+        /// <param name="index">индекс элемента</param>
+        /// <returns>Элемент с определенным индексом</returns>
+        /// <exception cref="IndexOutOfRangeException">Элементы с 
+        /// несуществующии индексами</exception>
+        public PersonBase FindPersonByIndex(int index)
+        {
+            int indexNumber = people.Count - 1;
+            if (indexNumber < index)
+            {
+                throw new IndexOutOfRangeException($"Элемента с индексом" +
+                    $" {index} не существует");
+            }
+            else
+            {
+                return people[index];
+            }
+        }
+
+        /// <summary>
+        /// Возвращает индекс элемента при наличие его в списке
+        /// </summary>
+        /// <param name="name">имя</param>
+        /// <returns>индекс</returns>
+        public int ReturnIndexOfperson(string name)
+        {
+            int returnName = people.FindIndex(r => r.Name == name);
+            return returnName;
+        }
+
+        /// <summary>
+        /// Очистка списка
+        /// </summary>
+        public void ClearPerson()
+        {
+            people.Clear();
+        }
+
+        /// <summary>
+        /// Получение количества элементов в списке
+        /// </summary>
+        public int CountPersonInList()
+        {
+            return people.Count();
+        }
+    }
+}
