47a3ca3bb89fda02a40d049e1bea25d2d5d59a34
AlenaPakhomova <alena.pahomova.99@mail.ru>
2023-05-09 22:40:55 +0700

    Новая ветка

---
+++ LR_4/LR_1/Program.cs
@@ -0,0 +1,210 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace LR_3 
+{
+    /// <summary>
+    /// Класс для выполнения программы
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args">параметры</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("Приветствуем нового работника " +
+                "нашей фирмы! \nДля ознакомлления с размерами " +
+                "оплаты труда нажмите любую клавишу...");
+            Console.ReadKey();
+
+            while (true)
+            {
+                Console.WriteLine("\nСпособы начисления зарплаты:" +
+                "\n1 - Оклад" +
+                "\n2 - Часовая тарифная ставка" +
+                "\n3 - Тарифная ставка" +
+                "\n4 - Завершить выбор");
+
+                Console.Write("\nВведите цифру из представленного " +
+                    "списка: ");
+                var consoleKey = Console.ReadLine();
+                switch (consoleKey)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("\tОплата по окладу");
+                            PrintWages(WagesBySalary());
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("\tОплата по часовой " +
+                                "тарифной ставке");
+                            PrintWages(WagesAtTheHourlyTariffRate());
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("\tОплата по тарифной " +
+                                "ставке");
+                            PrintWages(WagesAtTheTariffRate());
+                            break;
+                        }
+                    case "4":
+                        {
+                            Environment.Exit(4);
+                            break;
+                        }
+                    default:
+                        {                          
+                            Console.WriteLine("Попробуйте еще раз");
+                        }
+                        break;
+                }                    
+            }           
+        }
+
+        /// <summary>
+        /// Ввод данных для расчёта размера заработной платы 
+        /// по тарифной ставке
+        /// </summary>
+        /// <returns>данные о заработной плате</returns>
+        public static WageRate WagesAtTheTariffRate()
+        {
+            var wageRate = new WageRate();
+            var actions = new List<Action>()
+            {
+                new Action(() =>
+                {
+                    Console.Write("Количество отработанный дней в " +
+                        "месяце: ");
+                    wageRate.WorkingDays = ParseConsoleString();
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Тарифная ставка (рублей в день): ");
+                    wageRate.TariffRate = ParseConsoleString();
+                })
+            };
+            actions.ForEach(SetInformationFromConsole);
+            return wageRate;
+        }
+
+        /// <summary>
+        /// Ввод данных для расчёта размера заработной платы 
+        /// по часовой тарифной ставке
+        /// </summary>
+        /// <returns>данные о заработной плате</returns>
+        public static HourlyWageRate WagesAtTheHourlyTariffRate()
+        {
+            var hourlyWageRate = new HourlyWageRate();
+            var actions = new List<Action>()
+            {
+                new Action(() =>
+                {
+                    Console.Write("Размер часовой тарифной ставки " +
+                        "(рублей в час): ");
+                    hourlyWageRate.SizeOfTheHourlyTariffRate
+                    = ParseConsoleString();
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Количество отработанных часов: ");
+                    hourlyWageRate.WorkingHours = ParseConsoleString();
+                })
+            };
+            actions.ForEach(SetInformationFromConsole);
+            return hourlyWageRate;
+        }
+
+        /// <summary>
+        /// Ввод данных для расчёта размера заработной платы по окладу
+        /// </summary>
+        /// <returns>данные о заработной плате</returns>
+        public static Salary WagesBySalary()
+        {
+            var salary = new Salary();
+            var actions = new List<Action>()
+            {
+                new Action(() =>
+                {
+                    Console.Write("Размер оклада (в рублях): ");
+                    salary.SalaryAmount = ParseConsoleString();
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Количество фактически " +
+                        "отработанных дней: ");
+                    salary.WorkingDays = ParseConsoleString();
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Количество рабочих дней " +
+                        "в месяце: ");
+                    salary.DaysInMonth = ParseConsoleString();
+                }),
+            };
+            actions.ForEach(SetInformationFromConsole);
+            return salary;
+        }
+
+        /// <summary>
+        /// Вывод полученной информации на консоль
+        /// </summary>
+        /// <param name="value">заработная плата</param>
+        public static void PrintWages(WagesBase value)
+        {
+            Console.WriteLine($"Размер заработной платы: " +
+                $" {value.Wages()} рублей.");
+        }
+
+        /// <summary>
+        /// Чтение информации с консоли
+        /// </summary>
+        /// <returns>введенное слово</returns>
+        public static double ParseConsoleString()
+        {
+            return double.Parse(Console.ReadLine().Replace('.', ','));
+        }
+
+        /// <summary>
+        /// Получение введенной информации
+        /// </summary>
+        /// <param name="action">делегаты</param>
+        public static void SetInformationFromConsole(Action action)
+        {
+            while (true)
+            {
+                try
+                {
+                    action.Invoke();
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"\n{e.Message}\n");
+                }
+            }
+        }
+    }         
+}
+
+
+
+
+        
+  
+      
+    
---
+++ LR_4/Model/HourlyWageRate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Класс для оплаты по часовой тарифной ставке
+    /// </summary>
+    public class HourlyWageRate : WagesBase
+    {
+        /// <summary>
+        /// Размер часовой тарифной ставки
+        /// </summary>
+        private double _sizeOfTheHourlyTariffRate;
+
+        /// <summary>
+        /// Количество фактически отработанных часов
+        /// </summary>
+        private double _workingHours;
+
+        /// <summary>
+        /// Размер часовой тарифной ставки
+        /// </summary>
+        public double SizeOfTheHourlyTariffRate
+        {
+            get
+            {
+                return _sizeOfTheHourlyTariffRate;
+            }
+            set
+            {
+                CheckPositiveNumber(value);
+                _sizeOfTheHourlyTariffRate = value;
+            }
+
+        }
+
+        /// <summary>
+        /// Количество фактически отработанных часов
+        /// </summary>
+        public double WorkingHours
+        {
+            get
+            {
+                return _workingHours;
+            }
+            set
+            {
+                CheckPositiveNumber(value);
+                _workingHours = value;
+            }
+
+        }
+
+        /// <summary>
+        /// Вычисление зарплаты по часовой тарифной ставке
+        /// </summary>
+        public override double Wages() => 
+            _sizeOfTheHourlyTariffRate * _workingHours;
+    }
+}
